42a9c350ed5e6a1022fc48dc0615835a94b1e7b1
Tatanya Lynch <tatanyalynch@gmail.com>
2021-02-25 15:52:16 -0500

    Updated files

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_mail import Mail
+from .config import Config
+from flask_wtf.csrf import CSRFProtect
+
+csrf = CSRFProtect()
 
 app = Flask(__name__)
+app.config.from_object(Config)
+app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = '465'
+app.config['MAIL_USERNAME'] = '82a2db10bc2089'
+app.config['MAIL_PASSWORD'] = '0e941b762f2293'
+app.config['SECRET_KEY'] = 'spicy'
+
+mail = Mail(app)
+csrf.init_app(app)
 from app import views
